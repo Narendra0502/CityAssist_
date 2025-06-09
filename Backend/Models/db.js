08a98df03d2dd8fee1f54e8a0f6fd45c1a721959
Narendra Kaushik <156305888+Narendra0502@users.noreply.github.com>
2025-06-09 14:03:18 +0530

    Update db.js

--- Backend/Models/db.js
+++ Backend/Models/db.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose');
 
 const mongo_url = process.env.MONGODB_URI;
 
-mongoose.connect(mongo_url)
+const connectDB = async () => {
+    mongoose.connect(mongo_url)
     .then(() => {
         console.log('MongoDB Connected...');
     }).catch((err) => {
         console.log('MongoDB Connection Error: ', err);
     })
+}
+
+module.exports = connectDB;
